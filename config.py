74fbd5318b59203513e29550f52aa65556b1e51c
KUMSAL MÜZİK <kurulummm0@gmail.com>
2024-08-08 00:08:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+API_ID = int(getenv("API_ID", "29772838"))
+API_HASH = getenv("API_HASH", "15ce514f4f3fcea70d7a49dc5064b839")
+OWNER_ID = int(getenv("OWNER_ID", "7268753735"))
+STRING_SESSION = getenv("STRING_SESSION", "BAHGTCYASfB1FNbqfb16UWkNNoc978F0LYMLQ_TU89JNu0Z9LDm5PJgb51IO_K2ppaa3HQdvJS5iag_OhpG5u4eyxk3lMUCKkR_fmn2bFjIpgtdHHpC-RhshLPOcv4Xs46MJA4DiGcm8jqJd_C3ZKmw-BEUiiSpZWBr_1UBUHamLQAelCP131qCFECT25gN274zSjeYxEalERSWjcQl9NU-DpYMZ811B8TKj84KSXScP67hbj0gnlk4jO0qRz1VTHGTKL3ERWaLaSoju-jKI4f3TiFnK98312tJSLe-gaGIwRLGrEJg-XGgXd5Pps8z8GsAk1YUYrUQ8QYpgrjZCDUMJKNQDXAAAAAGxQGFHAA")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://kurt67143:nays@cluster0.vjg7bma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "gecemavisisohbett")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "kumsaldestekkanal")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "nazli_hanim_diyeceksiniz")
 
 # Random Start Images
 IMG = [
